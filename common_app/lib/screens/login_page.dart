b2caf11cbc160ff3a56d060e2c6e1e8f4caf615f
Daniel <danielms215@gmail.com>
2018-09-05 11:41:01 -0400

    Added logo
    ayy

--- common_app/lib/screens/login_page.dart
+++ common_app/lib/screens/login_page.dart
@@ -10,6 +10,16 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+
+    final logo = Hero(
+        tag: "hero",
+        child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 48.0,
+          child: Image.asset("assets/logo.png"),
+        )
+    );
+
     final email = TextFormField(
       keyboardType: TextInputType.emailAddress,
       autofocus: false,
@@ -65,6 +75,7 @@ class _LoginPageState extends State<LoginPage> {
           shrinkWrap: true,
           padding: EdgeInsets.only(left: 24.0, right: 24.0),
           children: <Widget>[
+            logo,
             SizedBox(height: 48.0),
             email,
             SizedBox(height: 8.0),
