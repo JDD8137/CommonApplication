ae3f800cf32d13ef40335f652c041c192d582b23
Daniel <danielms215@gmail.com>
2018-09-05 11:20:23 -0400

    Login Screen initial commit
    Added dummy login screen

--- common_app/lib/main.dart
+++ common_app/lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'screens/login_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  final routes = <String, WidgetBuilder>{
+    LoginPage.tag: (context) => LoginPage(),
+  };
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Common Application',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Common Application'),
-        ),
-        body: Center(
-          child: Text('African Common Application Blockchain'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+      routes:routes,
     );
   }
 }
